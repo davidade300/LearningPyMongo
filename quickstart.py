7c8cc7416d35846682801df00f2fac16879f2a38
davidade300 <iaderinho@outlook.com>
2024-05-27 18:14:33 -0300

    fixed a happy little mistake

--- quickstart.py
+++ quickstart.py
@@ -1,10 +1,14 @@
+"""
+Durante todo o ciclo de vida da aplicacao é interessante manter somente uma
+instancia do mongoclient, pois a criacão da mesma é custosa
+"""
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 from seed_documents import movie_list
 
 
-URI = "mongodb://davidade300:220498@192.168.124.64:27017/admin"
-client = MongoClient(URI, server_api=ServerApi(
+MONGODB_URI = "here's my connection string or smth like that"
+client = MongoClient(MONGODB_URI, server_api=ServerApi(
     version='1', strict=True, deprecation_errors=True))
 
 try:
